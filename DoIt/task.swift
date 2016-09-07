7289332270b883f702517726372143d13fc8fbcf
Tasi <tasi@Ninos-MacBook-Pro.local>
2016-09-07 10:43:12 +0100

    added segues

---
+++ DoIt/task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by Tasi on 07/09/2016.
+//  Copyright © 2016 Tasi. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
